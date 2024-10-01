19d5875902c1ae7cbd468c7f7de00a53642b0a24
ViniMacacari <macacarivinicius@gmail.com.br>
2024-10-01 13:01:17 -0300

    Navegação no home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(
+    private router: Router
+  ) { }
 
+  navegar(pagina: string): void {
+    this.router.navigate([pagina])
+  }
 }
