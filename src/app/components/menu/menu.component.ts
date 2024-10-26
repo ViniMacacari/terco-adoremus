ece000cb7048db9d485a9670d5f85e8f1768ad87
ViniMacacari <macacarivinicius@gmail.com>
2024-10-26 20:07:58 -0300

    Menu para abrir e fechar quando clicado em disp. móveis

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, HostListener, ElementRef } from '@angular/core'
 import { Router } from '@angular/router'
 
 @Component({
@@ -9,11 +9,37 @@ import { Router } from '@angular/router'
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
-  constructor(
-    private router: Router
-  ) { }
+  aberto = false
+
+  constructor(private router: Router, private el: ElementRef) { }
 
   navegar(pagina: string): void {
+    this.fechar()
     this.router.navigate([pagina])
+  }
+
+  alternar(): void {
+    const navbar = this.el.nativeElement.querySelector('#navbarResponsive')
+    if (navbar.classList.contains('show')) {
+      (window as any).bootstrap.Collapse.getInstance(navbar)?.hide() || new (window as any).bootstrap.Collapse(navbar).hide()
+    } else {
+      new (window as any).bootstrap.Collapse(navbar).show()
+    }
+  }
+
+  fechar(): void {
+    const navbar = this.el.nativeElement.querySelector('#navbarResponsive')
+    if (navbar.classList.contains('show')) {
+      (window as any).bootstrap.Collapse.getInstance(navbar)?.hide()
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickFora(event: Event) {
+    const target = event.target as HTMLElement
+    const clicouDentro = target.closest('.navbar')
+    if (!clicouDentro) {
+      this.fechar()
+    }
   }
 }
